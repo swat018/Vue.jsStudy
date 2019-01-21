75ddb0747421ce6a72c21c1f342f39348854b157
jinwoo park <swat018@gmail.com>
2019-01-21 16:42:16 +0900

    vue와 bootstrap, chart 라이브러리 연동

---
+++ bootstrap/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <button type="button" class="btn btn-primary btn-log">Lage button</button>
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import Chart from "./Chart.vue";
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  components: {
+    Chart
+  }
+};
+</script>
+
---
+++ bootstrap/src/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <dive id="container" style="width:100%; height:400px;"></dive>
+</template>
+
+<script>
+export default {
+  mounted() {
+    Highcharts.chart("container", {
+      chart: {
+        type: "bar"
+      },
+      title: {
+        text: "Fruit Consumption"
+      },
+      xAxis: {
+        categories: ["Apples", "Bananas", "Oranges"]
+      },
+      yAxis: {
+        title: "Fruit eaten"
+      },
+      series: [
+        {
+          name: "Jane",
+          data: [1, 0, 4]
+        },
+        {
+          name: "John",
+          data: [5, 7, 3]
+        }
+      ]
+    });
+  }
+};
+</script>
